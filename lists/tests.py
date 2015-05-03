c60f92f2eddfb1855e8940cc0a7aee11296bdd97
Holly L Malm <holly.malm5@gmail.com>
2015-05-03 16:06:52 -0700

    I am obeying the goat...

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,17 @@
 from django.test import TestCase
+from django.http import HttpRequest
+from django.core.urlresolvers import resolve 
+from lists.views import home_page
 
-class SimpleTest(TestCase):
-	def test_basic_addition(self):
-		"""Tests that 1 + 1 always equals 2."""
-		self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+
+	def test_root_url_resolves_to_home_pages_view(self):
+		found = resolve('/')
+		self.assertEqual(found.func, home_page)
+
+	# def test_home_page_returns_correct_html(self):
+	# 	request = HttpRequest()
+	# 	response = home_page(request)
+	# 	self.assertTrue(response.content.startswith('<html>'))
+	# 	self.assertIn('<title>To-Do lists</title', response.content)
+	# 	self.assertTrue(response.content.endswith('</html>'))
